193be9b89116e4a13a59c29d95c1328a6e6b455d
gryphonsft <abdbzd01@gmail.com>
2025-07-29 02:47:39 +0300

    Cihaz ekle fonksiyonu eklendi.

---
+++ Migrations/20250728212936_BrandModelSelectDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace itTicketSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class BrandModelSelectDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BrandModelSelectDB",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrandModelSelectDB", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrandModelSelectDB");
+        }
+    }
+}
